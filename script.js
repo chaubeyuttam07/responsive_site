28fbe6076edd30cec8a20772ed30b4ba41f42f01
Uttam Chaubey <154907428+chaubeyuttam07@users.noreply.github.com>
2025-01-21 18:44:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ function menuBtnChange() {
 }
 
 menuBtnChange();
+// Responsive adjustments for screen widths
+// function adjustPageSize() {
+//   const width = window.innerWidth;
+
+//   if (width >= 992 && width <= 1600) {
+//     document.body.style.transform = 'scale(0.9)';
+//     document.body.style.transformOrigin = 'top left';
+//   } else if (width >= 700 && width <= 767) {
+//     document.body.style.transform = 'scale(0.8)';
+//     document.body.style.transformOrigin = 'top left';
+//   } else if (width >= 600 && width < 700) {
+//     document.body.style.transform = 'scale(0.75)';
+//     document.body.style.transformOrigin = 'top left';
+//   } else if (width <= 600) {
+//     document.body.style.transform = 'scale(0.5)';
+//     document.body.style.transformOrigin = 'top left';
+//   } else {
+//     document.body.style.transform = 'scale(1)';
+//     document.body.style.transformOrigin = 'top left';
+//   }
+// }
+
+// window.addEventListener('resize', adjustPageSize);
+// adjustPageSize();
